6ceb37c96391b12a92ed8fa9b6c783c49b21d56c
Joe Grossberg <josephgrossberg+github@gmail.com>
2016-04-06 14:28:31 -0700

    adding comments

--- lib/fund_america/webhook_log.rb
+++ lib/fund_america/webhook_log.rb
@@ -2,9 +2,12 @@ module FundAmerica
   class WebhookLog
     class << self
 
-      # End point: https://apps.fundamerica.com/api/webhook_logs (GET)
+      # End point: https://apps.fundamerica.com/api/webhook_logs/?page=[num]&per=[num] (GET)
       # Usage: FundAmerica::WebhookLog.list
       # Output: Returns list of webhook_logs
+      # Params: (method defaults are the same as the current API defaults)
+      #  * page - page number of results; starts at 1, not 0
+      #  * per - number of results per page
       def list(page: 1, per: 25)
         request_uri = "/?page=#{page}&per=#{per}" if page
         API::request(:get, request_uri)
