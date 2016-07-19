189f3d93cef6d47f77ad20c262db8b5f7ef5297b
Johan Oskarsson <johan@oskarsson.nu>
2016-07-19 09:13:50 -0700

    Add customer agreement

---
+++ lib/fund_america/customer_agreement.rb
@@ -0,0 +1,21 @@
+module FundAmerica
+  class CustomerAgreement
+    class << self
+
+      # End point: https://apps.fundamerica.com/api/entities/:id/customer_agreement/edit (GET)
+      # Usage: FundAmerica::CustomerAgreement.edit(entity_id)
+      # Output: Returns the form details for the customer agreement
+      def edit(entity_id)
+        API::request(:get, FundAmerica.base_uri + "entities/#{entity_id}/customer_agreement/edit")
+      end
+
+      # End point: https://apps.fundamerica.com/api/entities/:id/customer_agreement (PATCH)
+      # Usage: FundAmerica::CustomerAgreement.update(entity_id, options)
+      # Output: Fill out the customer agreement
+      def update(entity_id, options)
+        API::request(:patch, FundAmerica.base_uri + "entities/#{entity_id}/customer_agreement", options)
+      end
+
+    end
+  end
+end
