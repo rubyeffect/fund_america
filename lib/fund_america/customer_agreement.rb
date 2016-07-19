d92a94c43c1bbdba9402e3489fa24e31373c435d
David Reid <dreid@dreid.org>
2016-07-19 09:29:24 -0700

    Add specs.

--- lib/fund_america/customer_agreement.rb
+++ lib/fund_america/customer_agreement.rb
@@ -6,14 +6,14 @@ module FundAmerica
       # Usage: FundAmerica::CustomerAgreement.edit(entity_id)
       # Output: Returns the form details for the customer agreement
       def edit(entity_id)
-        API::request(:get, FundAmerica.base_uri + "entities/#{entity_id}/customer_agreement/edit")
+        API::request(:get, "entities/#{entity_id}/customer_agreement/edit")
       end
 
       # End point: https://apps.fundamerica.com/api/entities/:id/customer_agreement (PATCH)
       # Usage: FundAmerica::CustomerAgreement.update(entity_id, options)
       # Output: Fill out the customer agreement
       def update(entity_id, options)
-        API::request(:patch, FundAmerica.base_uri + "entities/#{entity_id}/customer_agreement", options)
+        API::request(:patch, "entities/#{entity_id}/customer_agreement", options)
       end
 
     end
