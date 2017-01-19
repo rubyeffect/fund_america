634c8dc43bec21e7fd370ebb8f89cb17789c00d1
Yuri Zubov <y.zubov@sumatosoft.com>
2017-01-19 15:42:33 +0300

    Update fund_america.gemspec
    added rta_agreement
    
    added tech service agreement

--- lib/fund_america/rta_agreement.rb
+++ lib/fund_america/rta_agreement.rb
@@ -6,21 +6,21 @@ module FundAmerica
       # Usage: FundAmerica::RTAAgreement.list
       # Output: Returns list of rta_agreements
       def list
-        API::request(:get, FundAmerica.base_uri + 'rta_agreements')
+        API::request(:get, 'rta_agreements')
       end
 
       # End point: https://apps.fundamerica.com/api/rta_agreements (POST)
       # Usage: FundAmerica::RTAAgreement.create(options)
       # Output: Creates a new rta_agreement
       def create(options)
-        API::request(:post, FundAmerica.base_uri + 'rta_agreements', options)
+        API::request(:post, 'rta_agreements', options)
       end
 
       # End point: https://apps.fundamerica.com/api/rta_agreements/:id (GET)
       # Usage: FundAmerica::RTAAgreement.details(rta_agreement_id)
       # Output: Returns the details of an rta_agreement with matching id
       def details(rta_agreement_id)
-        API::request(:get, FundAmerica.base_uri + "rta_agreements/#{rta_agreement_id}")
+        API::request(:get, "rta_agreements/#{rta_agreement_id}")
       end
 
     end
