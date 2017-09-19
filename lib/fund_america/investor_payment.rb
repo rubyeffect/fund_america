25064759a32c216523196356834e2b7a8df195e7
Dan M <dan987@aol.com>
2017-09-19 18:39:25 -0400

    comments for investor_payment create

--- lib/fund_america/investor_payment.rb
+++ lib/fund_america/investor_payment.rb
@@ -16,8 +16,11 @@ module FundAmerica
         API::request(:get, "investor_payments/#{investor_payment_id}")
       end
 
+      # End point: https://apps.fundamerica.com/api/investor_payments (POST)
+      # Usage: FundAmerica::InvestorPayment.create 
+      # Output: Creates a new investor_payment
       def create(options = {})
-        API::request(:post, options)
+        API::request(:post, 'investor_payments', options)
       end 
 
     end
